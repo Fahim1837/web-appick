5a749be5734158d7336b1f6b54969c8a50ef1c60
Fahim1837 <fahimrb37@gmail.com>
2024-08-22 06:43:50 +0600

    useWindowDimensions hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import Header from './components/header'
+import useWindowDimensions from './hooks/useWindowDimension'
+
 function App() {
-    return <div className="app">
-      Hello World
-    </div>
+    // console.log(window)
+    // console.log(window.innerHeight, window.innerWidth)
+    // console.log(window.location)
+    // console.log(window.screen)
+    const { width, height } = useWindowDimensions()
+    console.log(width, height)
+    return (
+        <div className="app">
+            Hello World
+            {width < 600 && <h1>Hello Dear</h1>}
+            <Header />
+        </div>
+    )
 }
 
 export default App
