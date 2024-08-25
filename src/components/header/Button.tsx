e86b6cc87e51460df1109a02a2a6cd81730086c4
Fahim1837 <fahimrb37@gmail.com>
2024-08-25 17:27:21 +0600

    Menu Bar Created

--- src/components/header/Button.tsx
+++ src/components/header/Button.tsx
@@ -1,5 +1,7 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import menu from '../../assets/svg/menu_bar.svg'
+import Navbar from './Navbar'
+import { useState } from 'react'
 function Button1() {
     const navigate = useNavigate()
     return (
@@ -13,11 +15,29 @@ function Button1() {
 }
 
 export function Button2() {
+    const [isOpen, setIsOpen] = useState(false)
     const navigate = useNavigate()
     return (
-            <button className="py-2 rounded-lg" onClick={() => navigate('/register')}>
-                <img src= {menu} alt="Menu" />
+        <div className='relative'>
+            <button className="py-2 rounded-lg" onClick={() => setIsOpen(!isOpen)}>
+                <img src={menu} alt="Menu" />
             </button>
+            {isOpen && (<div
+                className='bg-white z-10 absolute top-10 right-1'
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <nav className="flex gap-[30px] list-none  sm:flex-col sm:gap-2.5 sm:leading-7 sm:text-xs sm:p-3">
+                    <Link to="/">Home</Link>
+                    <Link to="/courses">Courses</Link>
+                    <Link to="/subscribe">Subscribe</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/testimoni">Testimoni</Link>
+                </nav>
+                <button className="bg-secondary px-11 py-2 rounded-lg mb-2 mx-1" onClick={() => navigate('/register')}>
+                    Register
+                </button>
+            </div>)}
+        </div>
     )
 }
 
